967ed1c93f5cbafb1f21e6da5d0dc156f71d2e6a
Romulo Carvalho <romulocarvalho081@gmail.com>
2021-10-23 23:42:37 -0300

    add signOut in Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,13 +6,35 @@
       <v-spacer></v-spacer>
 
       <v-app-bar-nav-icon> </v-app-bar-nav-icon>
-      <v-btn @click="signOut">Deslogar</v-btn>
+      <v-menu left bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon v-bind="attrs" v-on="on">
+            <v-icon aria-hidden="false"> mdi-account </v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item>
+            <v-list-item-title
+              ><a class="purple--text">Perfil</a></v-list-item-title
+            >
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-title
+              ><a class="purple--text" @click="signOut"
+                >signOut</a
+              ></v-list-item-title
+            >
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
   </div>
 </template>
 
 
 <script>
+import { mdiAccount } from "@mdi/js";
 import {
   getAuth,
   GoogleAuthProvider,
@@ -22,6 +44,11 @@ import {
 } from "firebase/auth";
 export default {
   name: "Header",
+  data: () => ({
+    icons: {
+      mdiAccount,
+    },
+  }),
   methods: {
     signOut() {
       getAuth()
